d2f88afcbde9f96246de0aa62c69d4d95cc51fa6
langyan <langyan.lin@gmail.com>
2015-06-02 13:51:22 +0800

    io

---
+++ src/test/java/com/lin/common/io/NIOScatteringandGatheringTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.lin.common.io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author michaellin
+ *
+ */
+public class NIOScatteringandGatheringTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void createFiles(){
+		NIOScatteringandGathering nio = new NIOScatteringandGathering();
+		nio.createFiles("D:\\1.TXT");
+	}
+
+}
