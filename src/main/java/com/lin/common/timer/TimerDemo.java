86313b11d2bb209b07113fa03bc1f7fe440fc9f7
langyan <langyan.lin@gmail.com>
2018-05-30 09:31:59 +0800

    time

---
+++ src/main/java/com/lin/common/timer/TimerDemo.java
@@ -0,0 +1,23 @@
+package com.lin.common.timer;
+
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerDemo {
+
+	public static void main(String[] args) {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("执行任务:" + LocalDateTime.now());
+            }
+        };
+        Timer timer = new Timer();
+        // timerTask：需要执行的任务
+        // delay：延迟时间（以毫秒为单位）
+        // period：间隔时间（以毫秒为单位）
+        timer.schedule(timerTask, 5000, 3000);
+    }
+	
+}
